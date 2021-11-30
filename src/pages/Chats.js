8845f160fef079b4c30e045441b3f63c42ec0010
Iana Spivac <yspivac@gmail.com>
2021-11-30 23:51:06 +0200

    some fixes for dialog

--- src/pages/Chats.js
+++ src/pages/Chats.js
@@ -1,11 +1,12 @@
-import ChatMessages from "../components/ChatMessages/ChatMessages";
-import DialogComponent from "../components/Dialogs/DialogComponent";
+import ChatMessages from "../components/ChatMessages/ChatMessages"
+import DialogComponent from "../components/Dialogs/DialogComponent"
+import "./Chats.css"
 
 function Chats() {
     return (
         <div className="chats-wrapper">
             <DialogComponent/>
-           <ChatMessages/>
+            <ChatMessages/>
         </div>
     );
 }
