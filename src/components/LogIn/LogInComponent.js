f6b563b240de7a230506296c580f3c86c4c9ebab
DoniDaniela <doni.daniela@isa.utm.md>
2022-01-17 22:29:52 +0200

    media query

--- src/components/LogIn/LogInComponent.js
+++ src/components/LogIn/LogInComponent.js
@@ -1,21 +1,57 @@
-import React, {useState} from "react";
+//import React, {useState} from "react";
+//import './LogIn.css'
+//import logo from '../../images/fox-head 1.svg'
+//import {Link, useHistory} from "react-router-dom";
+//import {postLogin} from "../../services/postLogin";
+//export default function LogInComponent () {
+//    const [email, setEmail] = useState('');
+//    const [password, setPassword] = useState('');
+//    const history = useHistory();
+
+//    const submitHandler = (event) =>{
+//        event.preventDefault();
+
+//        const response = postLogin({email: email, password: password});
+//        localStorage.setItem('userId', response.data.id.toString());
+//        history.push('/chats');
+//    }
+import React, { useState } from "react";
 import './LogIn.css'
 import logo from '../../images/fox-head 1.svg'
-import {Link, useHistory} from "react-router-dom";
-import {postLogin} from "../../services/postLogin";
-export default function LogInComponent () {
+import { Link, useHistory } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { postLogin } from "../../services/postLogin";
+
+export default function LogInComponent() {
+    const dispatch = useDispatch();
+
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [invalidCredentials, setInvalidCredentials] = useState(false);
     const history = useHistory();
 
-    const submitHandler = (event) =>{
-        event.preventDefault();
 
-        const response = postLogin({email: email, password: password});
-        localStorage.setItem('userId', response.data.id.toString());
-        history.push('/chats');
+    const postHandler = async () => {
+        const response = await postLogin({ email: email, password: password });
+        return await response;
     }
 
+    const submitHandler = (event) => {
+        event.preventDefault()
+        postHandler().then(function (response) {
+            if (response.data !== null) {
+                dispatch({
+                    type: "LOGIN",
+                    payload: response.data.id.toString()
+                });
+                localStorage.setItem('userId', response.data.id.toString())
+                history.push('/chats');
+                setInvalidCredentials(false);
+            } else {
+                setInvalidCredentials(true);
+            }
+        })
+    };
     return(
         <div className="logInWrapper">
             <img src={logo} alt="Logo" className="logo"/>
